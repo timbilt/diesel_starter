378e7309ddadfe00366fb52ab03c19cabad52b9e
Delano Cooper <delanoscooper@gmail.com>
2024-08-18 15:07:16 -0400

    chore: wip

--- src/bin/run.rs
+++ src/bin/run.rs
@@ -1,6 +1,23 @@
 extern crate diesel_starter;
 
+use diesel::prelude::*;
+use diesel_starter::{connect, models::*, schema};
+
 fn main() {
-    let conn = diesel_starter::establish_connection();
-    println!("Hello, world!");
+    use schema::posts::dsl::*;
+
+    let connection: &mut PgConnection = &mut connect::establish_connection();
+    let results = posts
+        .filter(published.eq(true))
+        .limit(5)
+        .select(Post::as_select())
+        .load(connection)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("-----------\n");
+        println!("{}", post.body);
+    }
 }
